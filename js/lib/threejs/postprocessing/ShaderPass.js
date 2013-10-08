bf91021470e528feaa72c7f9c32ce79a00ab416f
Matt Cook <matt@zebradog.com>
2013-10-08 12:26:23 -0500

    Moved final output to seperate rendering target

--- js/lib/threejs/postprocessing/ShaderPass.js
+++ js/lib/threejs/postprocessing/ShaderPass.js
@@ -21,6 +21,18 @@ THREE.ShaderPass = function ( shader, textureID ) {
 	this.enabled = true;
 	this.needsSwap = true;
 	this.clear = false;
+  
+  /*hack to re-init EffectComposer
+  http://stackoverflow.com/a/14568449/580136
+  related to this bug:
+  https://github.com/mrdoob/three.js/issues/2951
+  applying this patch: 
+  https://gist.github.com/anonymous/b1e6f845dd79eaa5aa9a
+  */
+  this.camera = new THREE.OrthographicCamera( -1, 1, 1, -1, 0, 1 );
+  this.scene  = new THREE.Scene();
+  this.quad = new THREE.Mesh( new THREE.PlaneGeometry( 2, 2 ), null );
+  this.scene.add( this.quad );
 
 };
 
@@ -34,15 +46,18 @@ THREE.ShaderPass.prototype = {
 
 		}
 
-		THREE.EffectComposer.quad.material = this.material;
+		//THREE.EffectComposer.quad.material = this.material;
+    this.quad.material = this.material;
 
 		if ( this.renderToScreen ) {
 
-			renderer.render( THREE.EffectComposer.scene, THREE.EffectComposer.camera );
+			//renderer.render( THREE.EffectComposer.scene, THREE.EffectComposer.camera );
+      renderer.render( this.scene, this.camera );
 
 		} else {
 
-			renderer.render( THREE.EffectComposer.scene, THREE.EffectComposer.camera, writeBuffer, this.clear );
+			//renderer.render( THREE.EffectComposer.scene, THREE.EffectComposer.camera, writeBuffer, this.clear );
+      renderer.render( this.scene, this.camera, writeBuffer, this.clear );
 
 		}
 
